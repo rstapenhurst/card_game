257509945ded716b83b72ee409fd46da9a7d998d
Richard <richard.stapenhurst@gmail.com>
2014-08-10 18:49:39 +0000

    Added phase advance event

--- config/events.rb
+++ config/events.rb
@@ -1,4 +1,5 @@
 WebsocketRails::EventMap.describe do
 	subscribe :card_play_event, to: NoobController, with_method: :play_card
+	subscribe :phase_advance_event, to: NoobController, with_method: :advance_phase
 	subscribe :game_fetch_event, to: NoobController, with_method: :game_fetch
 end
