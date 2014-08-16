821606423ed32157031ff1944fdba529b7c0009d
Richard <richard.stapenhurst@gmail.com>
2014-08-16 21:23:54 +0000

    Fixing noob bug

--- app/controllers/noob_controller.rb
+++ app/controllers/noob_controller.rb
@@ -12,7 +12,7 @@ class NoobController < WebsocketRails::BaseController
 			output_event = event.slice(:type, :all_log, log_player.id == current_player.id ? :player_log : :opponent_log)
 			if event.has_key?('logs_by_id')
 				log_by_id = event.logs_by_id.select{|log| log_player.id == log.owner_id}
-				if log_by_id.count() == 0
+				if log_by_id.count() == 1
 					output_event.merge!({
 						log_by_id: log_by_id
 					})
